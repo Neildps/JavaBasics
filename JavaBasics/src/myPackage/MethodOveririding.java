5abd75987c03fff587f03bbe0b6b5902dbc2f7a8
dasne <dasne@Neil-PC>
2023-05-08 10:23:04 +0530

    3

--- JavaBasics/src/myPackage/MethodOveririding.java
+++ JavaBasics/src/myPackage/MethodOveririding.java
@@ -1,10 +1,41 @@
 package myPackage;
 
+class husband
+{
+	public String hname()
+	{
+	return "Pradeep";
+	}
+	public String title()
+	{
+	return "Mehta";
+	}
+}
+class wife extends husband
+{
+	public String wname()
+	{
+	return "Prabha";
+	}
+	public String title()		//here the title from husband class gets overridden by the wife class
+	{
+	return "Sharma";
+	}
+}
+
 public class MethodOveririding {
 
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//test text
+		
+		wife w = new wife();
+		husband h = new husband();
+				
+		System.out.println("Before marriage the name was :");
+		System.out.println(w.wname()+ " " + w.title());
+		System.out.println("After marriage the name was changed to :");
+		System.out.println(w.wname()+ " " + h.title());
+				
 	}
 
 }
--- JavaBasics/src/myPackage/MethodOverloading.java
+++ JavaBasics/src/myPackage/MethodOverloading.java
@@ -1,10 +1,31 @@
 package myPackage;
 
+class employee
+{
+	void emp() {
+		System.out.println("No details found");
+	}
+	void emp(String name) {
+		System.out.println("The name of the employee is " + name +".");
+	}
+	void emp(byte age) {
+		System.out.println("The age of the employee is " + age + " years.");
+	}
+	void emp(double  height) {
+		System.out.println("The height of the employee is " + height + " feet.");
+	}
+	void emp(int salary) {
+		System.out.println("The salary of the employee is " + salary + "/- Rs.");
+	}
+}
 public class MethodOverloading {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		employee intern = new employee();
+		intern.emp("Suresh Prabhakar");
+		intern.emp((byte)23);
+		intern.emp(5.6);
+		intern.emp(35000);
 	}
 
 }
